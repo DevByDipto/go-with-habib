f0d2c9b51987c3990b59ef954c600a6b32eb2b5d
DevByDipto <dipto.1900@gmail.com>
2026-01-28 23:04:16 +0600

    class-27 complete

---
+++ class-27/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+
+type User struct {
+	Name string // member variable /field / property
+	Age  int    
+}
+
+func main() {
+
+	
+	var user1 User
+
+
+	// Step 2: user1 এ value বসানো (instantiate করা)
+	user1 = User{
+		Name: "Habib",
+		Age:  30,
+	}
+
+	fmt.Println("Name:", user1.Name)
+	fmt.Println("Age:", user1.Age)
+
+	user2 := User{ // instance or object
+		Name: "Roki",
+		Age:  16,
+	}
+
+	
+	fmt.Println("Name:", user2.Name)
+	fmt.Println("Age:", user2.Age)
+}
