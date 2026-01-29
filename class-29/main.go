4853678e9161b154952f499e61c277de4eadcbca
DevByDipto <dipto.1900@gmail.com>
2026-01-29 12:41:49 +0600

    class 29 complete

---
+++ class-29/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+
+func main(){
+	var arr [2]int
+
+	arr[1] = 6
+	arr[0] = 3
+
+	fmt.Println(arr)
+}
+//----------------------------------------------
+
+// package main
+
+// import "fmt"
+
+// var arr2 = [3]string{"i","love","you"}
+
+// func main(){
+// 	arr := [2]int{3,6}
+
+// 	fmt.Println(arr)
+// 	fmt.Println(arr2)
+// }
+
+// task simulate the code
