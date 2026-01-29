c86be91d34d6559a5e642a9e3195d2d2bdb54134
DevByDipto <dipto.1900@gmail.com>
2026-01-29 18:35:12 +0600

    class 31 part 2 complete

--- class-31/main.go
+++ class-31/main.go
@@ -1,19 +1,41 @@
+//  package main
+
+// import "fmt"
+
+// func main(){
+// 	arr := [6]string {"this","is","a","go","interview","question"}
+// fmt.print(arr)
+
+// s := arr[1:4] // slice nijer moddhe 3 ta value rakhe 
+// // 1. pointer-> slice kora array er value gulor first value baa element er adress
+// // 2. length-> slice array er moddhe koita value baa element rakhlo jmn aikhane len=3
+// // 3. capacity-> jekhan theke clice kora shuru korse se value ta hoso oi original array te ar koita value baa element ase setar totall number jmn amader aikhetre cap=5
+
+// fmt.print(s)
+
+// }
+
+// task simulate : fmt.print(arr) ai porjonto to parbo then main stack fram e s name akta sell nibe tate akta info rakhbe ptn,len,cap then jokhon s print hobe tokhon dekbe main er moddhe s ase kina dekbe ase then s er value dekbe ptn somthing ar len-3 so ptn holo arr er slice start value oi jaiga theke 3 ta value tule ane print korbe. 
+// 
+
+//------------------------------------------------------------------------------------
+
  package main
 
 import "fmt"
 
-func main(){
+func main(){     //    18     19   20  21       22         23
 	arr := [6]string {"this","is","a","go","interview","question"}
 fmt.print(arr)
 
-s := arr[1:4] // slice nijer moddhe 3 ta value rakhe 
-// 1. pointer-> slice kora array er value gulor first value baa element er adress
-// 2. length-> slice array er moddhe koita value baa element rakhlo jmn aikhane len=3
-// 3. capacity-> jekhan theke clice kora shuru korse se value ta hoso oi original array te ar koita value baa element ase setar totall number jmn amader aikhetre cap=5
-
+s := arr[1:4] // ["is","a","go"]
 fmt.print(s)
 
+s1 := s[1:2] // ["a"]
+fmt.print(s1)
+fmt.print(lan(s1))
+fmt.print(cap(s1))
 }
 
-// task simulate : fmt.print(arr) ai porjonto to parbo then main stack fram e s name akta sell nibe tate akta info rakhbe ptn,len,cap then jokhon s print hobe tokhon dekbe main er moddhe s ase kina dekbe ase then s er value dekbe ptn somthing ar len-3 so ptn holo arr er slice start value oi jaiga theke 3 ta value tule ane print korbe. 
-// 
+// code simulation : fmt.print(s) ai porjonto to parbo. ar por jokhon dekha jabe j s1 := s[1,2] ache tokhon main er moddhe akta sell er nam hobe s1  then s er kase jabe ja akti struct ar tate ache ptn-19,cap-5,len-3 so se s er ptn dhore arr er kache jabe sekhan theke 1:2 unojai data niye asbe ar tate thakbe ptn-20,len-1,cap-4 name akta struct data thakbe.
+// -----------------------------------------------------------------------------
