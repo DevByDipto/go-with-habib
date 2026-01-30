910b68e1cbef8196dd284bf883c4eb39bbf66742
DevByDipto <dipto.1900@gmail.com>
2026-01-30 12:31:17 +0600

    class 32 complete

---
+++ class-32/main.go
@@ -0,0 +1,18 @@
+/*
+ram/voiatal memory/primary memory 
+hard disk/secondery memory/permanent memory
+cpu->+,_,*,/,&,|,! ai 7 ta oparation diye jabotiyo shob kaj kore thake
+0 -> current nai
+1-> current ase
+computer-> j jontro kichu gonona korte pare
+cpu-> 2 vage bivokto pointr unit o register set. pointer unit j khane 7 ta oparation hoi. example-image-1
+
+
+
+
+
+
+
+
+
+*/
