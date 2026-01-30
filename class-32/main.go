fa598135be220923e1f966d6f01e72d6d7a2259e
DevByDipto <dipto.1900@gmail.com>
2026-01-30 22:38:22 +0600

    class-32 again start

--- class-32/main.go
+++ class-32/main.go
@@ -1,12 +1,21 @@
 /*
-ram/voiatal memory/primary memory 
-hard disk/secondery memory/permanent memory
-cpu->+,_,*,/,&,|,! ai 7 ta oparation diye jabotiyo shob kaj kore thake
-0 -> current nai
-1-> current ase
-computer-> j jontro kichu gonona korte pare
-cpu-> 2 vage bivokto pointr unit o register set. pointer unit j khane 7 ta oparation hoi. example-image-1
+1. ram/voiatal memory/primary memory 
+2. hard disk/secondery memory/permanent memory
+3. cpu->+,_,*,/,&,|,! ai 7 ta oparation diye jabotiyo shob kaj kore thake
+4. 0 -> current nai
+5. 1-> current ase
+6. computer-> j jontro kichu gonona korte pare
+7. cpu-> 2 vage bivokto pointr unit o register set. pointer unit j khane 7 ta oparation hoi. example-image-1
+8. এটি কীভাবে কাজ করে?বিট কাজ করে বাইনারি পদ্ধতিতে। আমরা যদি বিটের সংখ্যা বাড়াই, তবে সংখার পরিমাণও বাড়ে। এর সূত্রটি হলো $2^n$ (যেখানে n হলো বিটের সংখ্যা)।১ বিট হলে: $2^1 = 2$টি সংখ্যা (০ এবং ১)২ বিট হলে: $2^2 = 4$টি সংখ্যা (০, ১, ২, ৩)৮ বিট (বা ১ বাইট) হলে: $2^8 = 256$টি ভিন্ন সংখ্যা প্রকাশ করা যায়।
+9. সহজভাবে মনে রাখার জন্য নিচের তালিকাটি মাথায় রাখতে পারো:
 
+৮ বিট = ১ বাইট (Byte)
+
+১০২৪ বাইট = ১ কিলোবাইট (KB)
+
+১০২৪ কিলোবাইট = ১ মেগাবাইট (MB)
+
+১০২৪ মেগাবাইট = ১ গিগাবাইট (GB) 
 
 
 
