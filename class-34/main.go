de7b0648cbb364ea5f0914a95f4f474d6cf4f6eb
DevByDipto <dipto.1900@gmail.com>
2026-01-31 14:00:31 +0600

    class 34 complete

---
+++ class-34/main.go
@@ -0,0 +1,26 @@
+/*
+cpu koi vage bivokti ?
+ans: image-2
+
+
+
+
+go filr HDD RAM CPU te kivabe exicute hoi ?-> image-1
+first e amra go file likhi then save click korle ta HDD e save hoi
+then build korle binary file build hoye tao HDD te save hoi
+then main.go file run korle HDD theke binary code ram e uthe jai jothakrome code,data segment etc te
+then cpu er program counter ram er next exicutable sell ke poin kore thake 
+then controll unit ase dekhe program counter kake point kore ase then controll unit dekhe j nirdisto akta cell ke poin kore ase (aikhane poin kore ase mane program counter er moddhe oi cell er adress ta rakha ase pore se adress controll unit follow kore fetch kore) tokhon poin kora cell er code controll unit ane ta instruction register e rakhe ar sate sate program counter er value ak barai dei
+then controll unit instruction register er coman gulo exicute kore ar coman er bivinno onshoke al bl cl e rakhe
+then controll unit pointer regidter ke bole al bl cl niye kaj korte o opraration calai then kono result pele ta dl e rekhe dei
+then controll unit ager process abar o repeat kore.
+
+
+process kii ?
+ram er codesegment,data segment,stack,heap o cpu er controll unit,arethmetic logic unit,program counter instrcution register er somonoye j kaj shomponno hoi take bole process
+
+process/vartual computer/logical computer akoi kotha keno?
+
+akta process er kase ja ja thake akta computer er kase o ta ta thake ar process consept ta exiest kore logical time e kichukkhoner jonno tai ake vartual computer/logical computer o bole.
+
+*/
