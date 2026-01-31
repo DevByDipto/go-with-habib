0bfdaee1410ce2ea3d7fa78d067a54855e3f4b8d
DevByDipto <dipto.1900@gmail.com>
2026-01-31 19:33:36 +0600

    update class 35

--- class-35/manin.go
+++ class-35/manin.go
@@ -7,4 +7,11 @@ bp-base pointer
 sp-stack pointer
 context swiching kii ?
 akta app er kisu comand run kore abar arekta app er kisu coman run kora baa akta contex theke arekta context e switch howa ke context swiching bole.
+multipale proces kivabe run hoi ?
+contex swiching process control block er saha jacche multipale proces run hoi.
+ process control block kii ?
+ pcb oparating system er akta aunsho . j block er vitor process er state save thake take process control block bole
+ cpute stae kake bole ?
+ একটি প্রসেস চলার সময় ওই মুহূর্তে সবগুলো রেজিস্টারের ভ্যালু, মেমোরির অবস্থা এবং প্রসেসটির বর্তমান কাজের ধাপ—এই সবকিছুর সমষ্টিগত লজিক্যাল অবস্থাকেই বলা হয় Process State বা Context।
+
 */
