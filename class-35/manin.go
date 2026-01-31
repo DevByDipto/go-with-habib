5a2fa285de0716ad23b989a0450c7ff61994fef5
DevByDipto <dipto.1900@gmail.com>
2026-01-31 19:07:36 +0600

    update class 35

---
+++ class-35/manin.go
@@ -0,0 +1,10 @@
+/*
+al-acomulator
+bl-base
+cl-counter
+stack frame er synoname function fram
+bp-base pointer
+sp-stack pointer
+context swiching kii ?
+akta app er kisu comand run kore abar arekta app er kisu coman run kora baa akta contex theke arekta context e switch howa ke context swiching bole.
+*/
