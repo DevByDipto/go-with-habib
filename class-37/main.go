f51d1f0189d916a8c3e6e0cd7619b0777f14e073
DevByDipto <dipto.1900@gmail.com>
2026-02-01 13:17:23 +0600

    complete class-37

---
+++ class-37/main.go
@@ -0,0 +1,11 @@
+/*
+paralalisum kii ?
+aksate jodi akadhik process run hoi ar vartual process akoi sate akadhik process exicute kore arthath akta task ke mondho rekhe arekta atask arthat context swiching nah kore tahole take paralalisme bole
+akta coputer e logical cpu ase 6 ta process ase 3 ta akhon ki context swiching hobe naki paralalisme hobe ?
+
+context swiching er counse ta kii ?
+context swiching e extra time cosume hoi aita contexswiching er akta cons
+core kii ?
+core dara onek kichu bujai tar moddhe akta bishoi bujai j core er upor base kore buja jai j akta computer er koita vartual cpu ase
+vartual processor vs vartual cpu vs logical cpu kii o parthokko ?
+*/
