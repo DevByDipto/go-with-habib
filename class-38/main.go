dfcd8a2f7b43dbc5dfe2b821ec6030fbc022d208
DevByDipto <dipto.1900@gmail.com>
2026-02-01 16:49:53 +0600

    class-38 complete

---
+++ class-38/main.go
@@ -0,0 +1,11 @@
+/*
+1. Hardware kii ?
+কম্পিউটারের RAM, HDD, CPU—এই সবকিছুকে একত্রে এবং আলাদাভাবেও Hardware (হার্ডওয়্যার) বলা হয়।
+
+সহজ কথায় বলতে গেলে: কম্পিউটারের যে অংশগুলো আপনি ছুঁতে পারেন (Physical parts) এবং যা শক্ত বা কঠিন পদার্থ দিয়ে তৈরি, সেগুলোই হার্ডওয়্যার।
+2. process er moddhe context switching kivabe hoi ?
+3. os threde vs process thred kii ?
+4.core kii ?
+5. jodi akadhik vartual cpu thake akadhik processor o tate akadhik thred thake to os full jinista ke manage kore kivabe
+6. thred thake koi ? process thred er vitor thake naki thred processer vitor ?
+*/
