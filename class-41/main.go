55b8795c25bfaf1101dcebe059053f4405027f0a
DevByDipto <dipto.1900@gmail.com>
2026-02-03 09:51:35 +0600

    complete class-41

--- class-41/main.go
+++ class-41/main.go
@@ -1,4 +1,15 @@
 /*
 thread kivabe manage hoi thread ke exicute kore?
+✅ CPU → OS চালায়
+   (CPU, OS kernel এর code execute করে)
 
+✅ OS (Kernel) → Thread manage করে
+   - কয়টা thread হবে
+   - কোন thread কখন চলবে
+   - thread এর priority কত
+   - thread কে কত CPU time পাবে
+   etc. এসব decision নেয়
+
+✅ CPU → Thread execute করে (OS এর instruction অনুযায়ী)
+   (Kernel যে thread select করে, CPU সেটা execute করে)
 */
