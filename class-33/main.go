2f235c80dd7e5c3b8e4667d50f9c74612067b590
DevByDipto <dipto.1900@gmail.com>
2026-02-03 22:22:53 +0600

    update class-33

--- class-33/main.go
+++ class-33/main.go
@@ -2,6 +2,20 @@
 1. amra computure e jekono oparation jokhon kori ta ki cpu er processing unit e exicute hoi.
 
 
+3. register set kii ?
+ans: একটি CPU-এর ভেতরে থাকা সমস্ত রেজিস্টারের সমষ্টি বা কালেকশনকে Register Set বলা হয়। অর্থাৎ, প্রসেসরের ভেতরে যতগুলো ছোট ছোট দ্রুতগতির মেমোরি ইউনিট (Register) আছে, তাদের পুরো গ্রুপটিকে একসাথে Register Set বলে।
+
+4. register kii ?
+ans: CPU-এর register set এর ভেতরে গাণিতিক (Mathematical) এবং যৌক্তিক (Logical) কাজগুলো করার সময় ডেটাকে সাময়িকভাবে (Temporarily) জমা রাখার জন্য যে ক্ষুদ্র abong অতি দ্রুতগতির মেমোরি ব্যবহার করা হয়, তাকে রেজিস্টার বলে .
+
+5. register e koto bit shonkha thake ar ta kiser upor depend kore ?
+ans: computer joto bit er register to to bit er hoi. computer 32 bit er hole protita register 32 bit er hobe.
+
+6. dhori database theke akta query kora holo query ta kothai exicute hoi ?
+ans: query ta cpu te exicute hoi ar processing unit exicute korai
+
+7. oparating system kake bole ?
+ans : অপারেটিং সিস্টেম (OS) হলো এমন একটি সিস্টেম ba সফটওয়্যার যা কম্পিউটারের হার্ডওয়্যার এবং ব্যবহারকারীর (User) মধ্যে যোগসূত্র স্থাপন করে এবং কম্পিউটারের সকল রিসোর্স (যেমন: CPU, মেমোরি, ইনপুট-আউটপুট ডিভাইস) নিয়ন্ত্রণ ও পরিচালনা করে
 
 
 */
