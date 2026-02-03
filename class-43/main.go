fcd56b5008888ae8f033942c8cb1e6ce478e3a40
DevByDipto <dipto.1900@gmail.com>
2026-02-03 13:50:50 +0600

    complete the class-43

---
+++ class-43/main.go
@@ -0,0 +1,12 @@
+/*
+rest kii ?
+representational state transfer.
+state kii ?
+state mane holo kono akta recourse er abostha.
+representational state?
+kono akta resource er current state ke akti formate e punorai present korte pari jmn json formate  ta holo representational state
+representational state transfer mane kii ?
+rest ar restfull service er deference kii ?
+api ?
+rest api ?
+*/
