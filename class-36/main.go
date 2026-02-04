738c63f9e9c3f0b1bbd45afcf4b7560de1b2e536
DevByDipto <dipto.1900@gmail.com>
2026-02-04 13:06:41 +0600

    update class-36

--- class-36/main.go
+++ class-36/main.go
@@ -1,11 +1,15 @@
 /*
-context swiching kii ?
-akta app er kisu comand run kore abar arekta app er kisu coman run kora baa akta contex theke arekta context e switch howa ke context swiching bole.
-multipale proces kivabe run hoi ?
-contex swiching process control block er saha jacche multipale proces run hoi.
- process control block kii ?
- pcb oparating system er akta aunsho . j block er vitor process er state save thake take process control block bole
- cpute stae kake bole ?
+1. context swiching kii ?
+ans: akta process er kisu comand run kore abar arekta process er kisu comand run kora baa akta contex theke arekta context e switch howa ke context swiching bole.
+2. multipale proces kivabe run hoi ?
+ans: contex swiching o paralisme er sahajj multipale proces run hoi.
+3. process control block kii ?
+ ans: pcb oparating system er akta aunsho . j block er vitor process er state save thake take process control block bole
+4. cpu te state kake bole ?
  একটি প্রসেস চলার সময় ওই মুহূর্তে সবগুলো রেজিস্টারের ভ্যালু, মেমোরির অবস্থা এবং প্রসেসটির বর্তমান কাজের ধাপ—এই সবকিছুর সমষ্টিগত লজিক্যাল অবস্থাকেই বলা হয় Process State বা Context।
+
+5. byte er uporbase kore ram e jaiga nei aibishoita bujaw?
+6. ami jokhon akadkhik software calai tokhon segulo akadhik process creat kore aii process gulo ke control kore ?
+ans: OS
 
 */
