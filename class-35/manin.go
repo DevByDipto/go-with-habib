2a4d587e2a6346df15fb06dd655cff9932d0737d
DevByDipto <dipto.1900@gmail.com>
2026-02-04 14:20:57 +0600

    update class-35

--- class-35/manin.go
+++ class-35/manin.go
@@ -1,7 +1,23 @@
 /*
-al-acomulator
-bl-base
-cl-counter
-stack frame er synoname function fram
-bp-base pointer
-sp-stack pointer
+1. stack frame er synoname function fram
+2. akta ram er bivinno cell e j local variable o argument value gulo rakha hoi se gulo dorkar porle kivabe khuje ber kora hoi ?
+ans: register set er base pointer er adress value  sate nirdishoto value jog biyog kore ber kora hoi.
+
+*/
+
+package main
+
+import "fmt"
+
+func add(x int, y int) int {
+	var res int
+	res = x + y
+	return res
+}
+
+func main() {
+	a := 10
+	sum := add(a, 4)
+	fmt.Println(sum)
+}
+// task - simulate how it work using sp bp
