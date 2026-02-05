5eaea7ec042a7fe93b60a4b71379130015bcbd9e
DevByDipto <dipto.1900@gmail.com>
2026-02-05 23:13:31 +0600

    class complete

--- class-49/mani.go
+++ class-49/mani.go
@@ -1,3 +1,5 @@
 /*
-
+1. HTTP OPTIONS method ?
+HTTP OPTIONS method হলো একটি preflight request, যা browser ব্যবহার করে কোনো নির্দিষ্ট URL বা server-এ কী কী HTTP request method (যেমন GET, POST, PUT, DELETE ইত্যাদি) ব্যবহার করা যাবে—এই তথ্য জানার জন্য। এটি একই সাথে complex headers বা non-simple request অনুমোদিত কিনা তা যাচাই করে এবং কোনো আসল কাজ (data নেওয়া বা পাঠানো) না করেই নিরাপত্তা নিশ্চিত করে—এভাবে browser ও server দুটোকেই সুরক্ষিত রাখে।
+2. preflight request kake bole ?
 */
