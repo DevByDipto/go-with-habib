5819b64ec4ee09e4c016b3e8be17ba52da4cd037
DevByDipto <dipto.1900@gmail.com>
2026-02-08 13:24:53 +0600

    start class-52

---
+++ class-52/cmd/serve.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"ecommerce/global_router"
+	"ecommerce/handlers"
+	"fmt"
+	"net/http"
+)
+
+func Serve() {
+	mux := http.NewServeMux()
+
+	mux.Handle("GET /products", http.HandlerFunc(handlers.GetProducts)) // route
+	mux.Handle("POST /create-products", http.HandlerFunc(handlers.CreateProduct))
+
+	fmt.Println("server runing 8080")
+
+	globalRouter := global_router.GlobalRouter(mux)
+	err := http.ListenAndServe(":8080", globalRouter)
+
+	if err != nil {
+		fmt.Println("Error starting the server", err)
+	}
+}
