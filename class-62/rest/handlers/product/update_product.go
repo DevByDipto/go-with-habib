64722fdd0b2aea48d18819802c574e8b790dd91d
DevByDipto <dipto.1900@gmail.com>
2026-02-11 12:10:46 +0600

    start class-62 again

---
+++ class-62/rest/handlers/product/update_product.go
@@ -0,0 +1,38 @@
+package product
+
+import (
+	"ecommerce/database"
+	"ecommerce/util"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func (h *Handler) UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	// URL থেকে আইডি নেওয়া
+	productIDStr := r.PathValue("id")
+
+	// স্ট্রিং আইডিকে ইন্টিজারে রূপান্তর
+	pId, err := strconv.Atoi(productIDStr)
+	if err != nil {
+		http.Error(w, "Please give me a valid product id", 400)
+		return
+	}
+
+	var newProduct database.Product
+	decoder := json.NewDecoder(r.Body)
+	err = decoder.Decode(&newProduct)
+
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Plz give me valid json", 400)
+		return
+	}
+	newProduct.ID = pId
+	// ডাটাবেস থেকে প্রোডাক্ট খোঁজা
+	database.Update(newProduct)
+
+	// সফল হলে ডাটা পাঠানো
+	util.SendData(w, "product update successfull", 201)
+}
